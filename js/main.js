6476ed37e629e1a7b5d14586a983381c374f9262
umashri-nagal-beamsuntory <umashri.nagal.metacube@beamsuntory.com>
2022-10-17 12:06:00 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(function(){
+  $(".testimonial-carousel").owlCarousel({
+    items: 2,
+    autoplay:1000,
+    loop:true,
+    dots:true,
+    responsive:true,
+    responsive: {
+      0:{
+        items:1
+      },
+      600:{
+        item:1
+      },
+      1000:{
+        item:2
+      }
+    }
+  });
+});
+$(function(){
+  $(".industry-carousel").owlCarousel({
+    items: 4,
+    autoplay:false,
+    loop:true,
+    dots:true,
+    responsive:true,
+    responsive: {
+      0:{
+        items:1
+      },
+      600:{
+        item:1
+      },
+      1000:{
+        item:4
+      }
+    }
+  });
+});
+
+$(window).scroll(function(){
+$('nav').toggleClass('scrolled',$(this).scrollTop()>630);
+});
+
+  
